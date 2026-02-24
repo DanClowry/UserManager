cf7c3a6d713be3de75a72fee14f3fc5a5f2e307c
Daniel Clowry <dan@clowry.au>
2026-02-24 21:15:00 +1100

    Update Program.cs

--- UserManager/Program.cs
+++ UserManager/Program.cs
@@ -10,13 +10,11 @@ using Microsoft.Identity.Web.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddAuthorization(options =>
 {
-    // By default, all incoming requests will be authorized according to the default policy
     options.FallbackPolicy = options.DefaultPolicy;
 });
 builder.Services.AddRazorPages()
