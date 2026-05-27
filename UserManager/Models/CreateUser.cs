31a31f03a5c8409a2000950696b707a715c7d50b
Daniel Clowry <dan@clowry.au>
2026-05-27 20:39:00 +1000

    Update CreateUser.cs

--- UserManager/Models/CreateUser.cs
+++ UserManager/Models/CreateUser.cs
@@ -6,6 +6,7 @@
         public string FirstName { get; set; }
         // lname
         public string LastName { get; set; }
+        // middle name
         public string MiddleName { get; set; }
         public int Age { get; set; }
     }
